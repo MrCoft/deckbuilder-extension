cc671019b0378a05333a7191ce90af04837ebd3b
MrCoft <devcoft@gmail.com>
2020-06-19 20:16:50 +0200

    ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,14 @@ module.exports = {
     webextensions: true,
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    // https://github.com/vuejs/eslint-plugin-vue#bulb-rules
+    'plugin:vue/recommended',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard',
+    // https://github.com/prettier/eslint-config-prettier
+    'prettier',
+    'prettier/standard',
+    'prettier/vue',
   ],
   // required to lint *.vue files
   plugins: [
